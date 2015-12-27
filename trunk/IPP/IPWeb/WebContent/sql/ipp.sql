47ee2bf1c28b76742baaf47269426053a3d7d92c
Deanna <qihuitoday@126.com>
2015-12-27 20:13:22 +0800

    Add some more fields。

--- trunk/IPP/IPWeb/WebContent/sql/ipp.sql
+++ trunk/IPP/IPWeb/WebContent/sql/ipp.sql
@@ -1,31 +1,39 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : Localhost
-Source Server Version : 50509
+Source Server         : localhost
+Source Server Version : 50521
 Source Host           : localhost:3306
-Source Database       : ssh
+Source Database       : ipp
 
 Target Server Type    : MYSQL
-Target Server Version : 50509
+Target Server Version : 50521
 File Encoding         : 65001
 
-Date: 2011-07-03 22:12:07
+Date: 2015-12-12 18:51:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `employee`
+-- Table structure for `ip_employee`
 -- ----------------------------
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE `employee` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `ip_employee`;
+CREATE TABLE `ip_employee` (
+  `ID` int(11) NOT NULL,
+  `EMPLOYEE_NAME` varchar(50) NOT NULL,
+  `ENGLISH_NAME` varchar(50) DEFAULT NULL,
+  `GENDER` varchar(4) DEFAULT NULL,
+  `MAIL` varchar(50) DEFAULT NULL,
+  `ADDRESS` varchar(100) DEFAULT NULL,
+  `MOBILE_NUMBER` varchar(20) DEFAULT NULL,
+  `CREATE_TIME` datetime NOT NULL,
+  `CREATE_BY` varchar(50) DEFAULT NULL,
+  `MODIFY_TIME` date DEFAULT NULL,
+  `MODIFY_BY` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of employee
+-- Records of ip_employee
 -- ----------------------------
